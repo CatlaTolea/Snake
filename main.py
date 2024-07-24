8500cc03948f1d9746bf5b58fba26a8ae852ed01
CatlaTolea <catlabuga.anatol@gmail.com>
2024-07-24 21:22:19 +0300

    Going to create a snake game.
    Added a basic surface, loaded a block and created draw function to draw it and then move it.
    Changed everything in OOP to make it more readable and easier to understand.
    Made the bolck to move in every direction by itself, we chose direction and it moves.
    Made the snalke longer and worte hoee the bolcks will move.
    Also draw the apple.
    Added score and collision fucntion,

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
 
@@ -16,15 +17,26 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,19)*SIZE
+
 
 class Snake:
     def __init__(self, parent_screen, length):
-        self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert()
+        self.direction = "down"
+
+        self.length = length
         self.x = [SIZE]*length
         self.y = [SIZE]*length
-        self.direction = "down"
+
+    def increase_length(self):
+        self.length+=1
+        self.x.append(-1)
+        self.y.append(-1)
+
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
@@ -62,25 +74,42 @@ class Snake:
             self.y[0] += SIZE
 
         self.draw()
-ghff
-
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((800, 600))
+        self.surface = pygame.display.set_mode((1000, 800))
         #     setmode is initializing your game window, you put the size
         self.surface.fill((110, 110, 5))
         # fill gives collors in RGB
-        self.snake = Snake(self.surface, 6)
+        self.snake = Snake(self.surface, 1)
         self.snake.draw()
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and 1 < y2 + SIZE:
+                return True
+
+        return False
+
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont("arial", 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200, 200, 200))
+        self.surface.blit(score,(800, 10))
+
     def run(self):
         running = True
 
